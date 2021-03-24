b2dfc996c1657b2084e391f3fd954593fdf4a422
Julius <johncrist2000@gmail.com>
2021-03-25 00:30:05 +0900

    generating fake filtered api

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -15,8 +15,10 @@ export type TestDataType = {
     no: number
     name: string
     color: string
+    closed?: boolean
   }
   name: string
+  writerNo: number
   writerName: string
   startDate: Date | string
   endDate?: Date | string
@@ -24,7 +26,11 @@ export type TestDataType = {
   subEndDate?: Date | string
   members?: Array<{
     no: number
+    name?: string
     email: string
+  }>
+  managers?: Array<{
+    no: number
   }>
 }
 
@@ -39,20 +45,101 @@ export type TestIconDataType = {
   channel?: {
     no: number
     name: string
+    closed?: boolean
   }
+  writerNo?: number
   writerName?: string
+  managers?: Array<{
+    no: number
+  }>
   cardName?: string
   done?: boolean
+  closed?: boolean
   members?: Array<{
     no: number
+    name?: string
     email: string
   }>
+}
+
+const memberFilter = <T extends TestDataType | TestIconDataType>(
+  filter: FilterType,
+  data: T,
+  baseFilter: boolean,
+) => {
+  if (!filter.member) return baseFilter
+  const { no, name, email, duty } = filter.member
+  if (!no && !name && !email && !duty) return baseFilter
+
+  const result = no
+    ? data.members?.some((member) => member.no === no) && baseFilter
+    : name
+    ? data.members?.some(
+        (member) =>
+          String(member.name)
+            .trim()
+            .toLowerCase()
+            .indexOf(name.trim().toLowerCase()) > -1,
+      ) && baseFilter
+    : email
+    ? data.members?.some(
+        (member) =>
+          member.email
+            .trim()
+            .toLowerCase()
+            .indexOf(email.trim().toLowerCase()) > -1,
+      ) && baseFilter
+    : baseFilter
+}
+
+const channelFilter = <T extends TestDataType | TestIconDataType>(
+  filter: FilterType,
+  data: T,
+  baseFilter: boolean,
+) => {
+  const result = filter.channel.no
+    ? data.channel?.no === filter.channel.no && baseFilter
+    : filter.channel.label
+    ? String(data.channel?.name)
+        .trim()
+        .toLowerCase()
+        .indexOf(filter.channel.label.trim().toLowerCase()) > -1 && baseFilter
+    : baseFilter
+
+  return filter.channel.closed === undefined
+    ? result
+    : filter.channel.closed === !!data.channel?.closed && result
 }
 
 export const testScheduleApi = (filter?: FilterType) => {
   return new Promise<TestDataType[]>((resolve) => {
     setTimeout(() => {
-      resolve(testScheduleData)
+      resolve(
+        filter
+          ? filter.schedule.show
+            ? testScheduleData.filter((data) => {
+                if (filter.schedule.no) {
+                  return channelFilter(
+                    filter,
+                    data,
+                    data.no === filter.schedule.no,
+                  )
+                }
+                if (filter.schedule.label) {
+                  return channelFilter(
+                    filter,
+                    data,
+                    data.name
+                      .trim()
+                      .toLowerCase()
+                      .indexOf(filter.schedule.label.trim().toLowerCase()) > -1,
+                  )
+                }
+                return channelFilter(filter, data, true)
+              })
+            : []
+          : testScheduleData,
+      )
     }, 2000)
   })
 }
@@ -63,8 +150,115 @@ export const testIconApi = (filter?: FilterType) => {
     cards: TestIconDataType[]
     todos: TestIconDataType[]
   }>((resolve) => {
+    const cardFilter = (
+      filter: FilterType,
+      data: TestIconDataType,
+      baseFilter: boolean,
+    ) => {
+      const result = filter.card.no
+        ? data.no === filter.card.no && baseFilter
+        : filter.card.label
+        ? String(data.name)
+            .trim()
+            .toLowerCase()
+            .indexOf(filter.card.label.trim().toLowerCase()) > -1 && baseFilter
+        : baseFilter
+
+      return filter.card.closed === undefined
+        ? result
+        : filter.card.closed === !!data.closed && result
+    }
+
     setTimeout(() => {
-      resolve(testIconData)
+      resolve(
+        filter
+          ? {
+              ...testIconData,
+              channels: filter.channel.show
+                ? testIconData.channels.filter((data) => {
+                    if (filter.channel.no) {
+                      return filter.channel.closed === undefined
+                        ? data.no === filter.channel.no
+                        : !!data.closed === filter.channel.closed &&
+                            data.no === filter.channel.no
+                    }
+                    if (filter.channel.label) {
+                      return filter.channel.closed === undefined
+                        ? data.name
+                            .trim()
+                            .toLowerCase()
+                            .indexOf(
+                              filter.channel.label.trim().toLowerCase(),
+                            ) > -1
+                        : !!data.closed === filter.channel.closed &&
+                            data.name
+                              .trim()
+                              .toLowerCase()
+                              .indexOf(
+                                filter.channel.label.trim().toLowerCase(),
+                              ) > -1
+                    }
+                    return filter.channel.closed === undefined
+                      ? true
+                      : filter.channel.closed === !!data.closed
+                  })
+                : [],
+              cards: filter.card.show
+                ? testIconData.cards.filter((data) => {
+                    if (filter.card.no) {
+                      return channelFilter(
+                        filter,
+                        data,
+                        data.no === filter.card.no,
+                      )
+                    }
+                    if (filter.card.label) {
+                      return channelFilter(
+                        filter,
+                        data,
+                        data.name
+                          .trim()
+                          .toLowerCase()
+                          .indexOf(filter.card.label.trim().toLowerCase()) > -1,
+                      )
+                    }
+                    return channelFilter(filter, data, true)
+                  })
+                : [],
+              todos: filter.todo.show
+                ? testIconData.todos.filter((data) => {
+                    if (filter.todo.no) {
+                      return cardFilter(
+                        filter,
+                        data,
+                        channelFilter(filter, data, data.no === filter.todo.no),
+                      )
+                    }
+                    if (filter.todo.label) {
+                      return cardFilter(
+                        filter,
+                        data,
+                        channelFilter(
+                          filter,
+                          data,
+                          data.name
+                            .trim()
+                            .toLowerCase()
+                            .indexOf(filter.todo.label.trim().toLowerCase()) >
+                            -1,
+                        ),
+                      )
+                    }
+                    return cardFilter(
+                      filter,
+                      data,
+                      channelFilter(filter, data, true),
+                    )
+                  })
+                : [],
+            }
+          : testIconData,
+      )
     }, 2000)
   })
 }
